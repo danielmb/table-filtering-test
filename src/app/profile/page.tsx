5e68afb4334af34b544882c90f3cab403ca8e234
Daniel Mathisen Breivik <danbre14@nfk.no>
2025-01-25 00:06:13 +0100

    Add OrganizationSwitcher to profile page for organization management

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { SignedIn, UserProfile } from '@clerk/nextjs';
+import { OrganizationSwitcher, SignedIn, UserProfile } from '@clerk/nextjs';
 const ProfilePage: React.FC = async () => {
   return (
     <div>
       {/* {user.orgPermissions} */}
       <SignedIn>
-        {/* <OrganizationSwitcher /> */}
+        <OrganizationSwitcher />
         <UserProfile routing="hash" />
       </SignedIn>
     </div>
