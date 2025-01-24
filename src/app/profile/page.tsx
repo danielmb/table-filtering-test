37d284654616e3f53e321341c5726e23b9681705
Daniel Mathisen Breivik <danbre14@nfk.no>
2025-01-25 00:06:59 +0100

    Refactor profile page layout for improved styling and alignment

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { OrganizationSwitcher, SignedIn, UserProfile } from '@clerk/nextjs';
 const ProfilePage: React.FC = async () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center w-full h-full space-y-4">
       {/* {user.orgPermissions} */}
       <SignedIn>
         <OrganizationSwitcher />
